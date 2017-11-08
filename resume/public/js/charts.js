77558a9dd700f3f4cdc44cc7a5826817c7985432
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-11-08 07:33:23 +0000

    Added horizontal bar chart to resume

---
+++ resume/public/js/charts.js
@@ -0,0 +1,48 @@
+const ctx = document.getElementById('horizontalBar').getContext('2d');
+
+const chart = new Chart(ctx, {
+  // Type of chart
+  type: 'horizontalBar',
+
+  // data
+  data: {
+    labels: ["Accountancy", "SQL", "Javascript", "NoSQL", "HTML5", "CSS3", "React"],
+    datasets: [{
+      backgroundColor: '#2facf9',
+      borderColor: '#2facf9',
+      data: [100, 87.5, 75, 70, 65, 57.5, 50],
+    },{
+      backgroundColor: '#EDEDED',
+      borderColor: '#EDEDED',
+      data: [0, 12.5, 25, 30, 35, 42.5, 50],
+    }]
+  },
+
+  // Configuration
+  options: {
+    tooltips: false,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        stacked: true,
+        display: false,
+        gridLines: {
+          drawBorder: false,
+          display: false,
+        },
+        ticks: {
+          suggestedMin: 0, // Will be Zero unless there is a lower value
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          drawBorder: false,
+          display: false,
+        },
+        stacked: true,
+      }]
+    }
+  }
+});

2d11f0df9a2ea487dcf296041fb7df0ff145508c
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-11-12 15:43:04 +0000

    added doughnut chart with interior text.

---
+++ resume/public/js/doughnuts.js
@@ -0,0 +1,42 @@
+const data = {
+  datasets: [
+    {
+      data: [300, 50],
+      backgroundColor: [
+        "#2facf9",
+        "#fff"
+      ],
+    }]
+};
+
+var promisedDeliveryChart = new Chart(document.getElementById('doughnuts'), {
+  type: 'doughnut',
+  data: data,
+  options: {
+    cutoutPercentage: 75,
+  	responsive: true,
+    legend: {
+      display: false
+    }
+  }
+});
+
+Chart.pluginService.register({
+  beforeDraw: function(chart) {
+    var width = chart.chart.width,
+        height = chart.chart.height,
+        ctx = chart.chart.ctx;
+
+    ctx.restore();
+    var fontSize = (height / 114).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+
+    var text = "75%",
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height / 2;
+
+    ctx.fillText(text, textX, textY);
+    ctx.save();
+  }
+});

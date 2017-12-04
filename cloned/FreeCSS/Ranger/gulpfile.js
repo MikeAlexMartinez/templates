dbbb908cdcd0c4ceca238544b8f5e8b11c535767
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-12-04 08:56:38 +0000

    amended directory structure and added gulp js workflow

--- cloned/FreeCSS/Ranger/gulpfile.js
+++ cloned/FreeCSS/Ranger/gulpfile.js
@@ -1,13 +1,75 @@
 'use strict';
 
+const browserify = require('browserify');
 const gulp = require('gulp');
+
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
+const uglify = require('gulp-uglify');
+
 const sass = require('gulp-sass');
+
+const gutil = require('gulp-util');
 const sourcemaps = require('gulp-sourcemaps');
 
+/**
+ * The JS gulp task creates two file, one with my source code which will change more
+ * frequently and another for vendor files which will allow for longer caching of files.
+ */
+
+/* JS vendor libs */
+const vendorsJs = ['jquery','waypoints/lib/noframework.waypoints','owl.carousel'];
+
+gulp.task('js:vendor', () => {
+  // set up the browserify instance on a task basis
+  const b = browserify({
+    debug: true
+  });
+
+  // require all libs from vendors array
+  vendorsJs.forEach(lib => {
+    b.require(lib);
+  });
+
+  b.bundle()
+    .pipe(source('vendor.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./MyCode/dist/js/'));  
+
+});
+
+gulp.task('js:app', () => {
+  browserify({
+    entries: ['./MyCode/src/js/main.js'],
+    extensions: ['.js'],
+    debug: true
+  })
+    .external(vendorsJs)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./MyCode/dist/js/'))
+});
+
+/** 
+ *  Sass compiling gulp task allows for including node_modules and also writes
+ *  sourcmaps to allow for easier debugging.
+ **/
 gulp.task('sass', function() {
   return gulp.src('./MyCode/src/sass/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./MyCode/dist/css'));
 });
@@ -18,6 +80,4 @@ gulp.task('watch', function() {
             ], ['sass']);
 });
 
-gulp.task('default', function() {
-  gulp.start("sass");
-});
+gulp.task('default', ['sass','js:app', 'js:vendor']);
--- cloned/FreeCSS/Ranger/index.js
+++ cloned/FreeCSS/Ranger/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 // initiate express app
 const app = express();
@@ -10,13 +12,19 @@ app.set('views', './MyCode/dist/templates');
 // set template engine to pug
 app.set('view engine', 'pug');
 
+// Allow for serving of favicons
+
+const faviconPath = path.join(__dirname,'MyCode','dist','images','favicon.ico');
+console.log(faviconPath);
+app.use(favicon(faviconPath));
+
 // This is where static files are served from
 app.use(express.static('./MyCode/dist'));
 
 // home route
 app.get('/', function homePage(req, res) {
   
-  res.render('home', {title: "Ranger - Home"});
+  res.render('home', {title: "RangerClone - Home"});
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
